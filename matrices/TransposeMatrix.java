1ff9047f1bc34c2522417a211547ff0b2c6b1009
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2024-12-20 11:13:30 +0100

    initial

--- matrices/TransposeMatrix.java
+++ matrices/TransposeMatrix.java
@@ -1,23 +1,19 @@
-public class MatrixElementWise {
+public class TransposeMatrix {
     public static void main(String[] args) {
         int[][] A = {
             {1, 2, 3},
             {4, 5, 6}
         };
 
-        int[][] B = {
-            {7, 8, 9},
-            {10, 11, 12}
-        };
-
         int rows = A.length;
         int cols = A[0].length;
-        int[][] C = new int[rows][cols];
+        int[][] T = new int[cols][rows];
 
-        // Element-wise addition
+        // Transpose logic
         TODO
 
-        // Print result matrix
+        // Print transposed matrix
+        System.out.println("Transposed Matrix:");
         TODO
     }
 }
