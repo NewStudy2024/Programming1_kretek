3dfdd2c912a02b08046ae56c90adffd48aa639d2
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2024-12-20 12:07:18 +0100

    New directory for the different trees im doing

---
+++ christmas_trees/ChristmasTree.java
@@ -0,0 +1,20 @@
+public class ChristmasTree {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("**");
+        }
+    }
+}
