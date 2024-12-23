8bf194b3e825984c634daef889f9413aca8f4b16
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2024-12-23 15:37:48 +0100

    New directory for the different trees im doing,

--- christmas_trees/ChristmasTree.java
+++ christmas_trees/ChristmasTree.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class ChristmasTree {
     public static void main(String[] args) {
-        int n = 6;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the tree you want to create: ");
+        int n = scanner.nextInt();
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("*");
+                if (i == 0) {
+                    System.out.print("+");
+                } else {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < n - 1; j++) {
