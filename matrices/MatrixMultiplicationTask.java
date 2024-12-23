606800f66a57279b5910878d95cc221e4739ccb1
Ali Omran <ali.omran.24@lehre.mosbach.dhbw.de>
2024-12-23 15:16:28 +0100

    Update and rename MatrixMultiplicationTask to MatrixMultiplicationTask.java

---
+++ matrices/MatrixMultiplicationTask.java
@@ -0,0 +1,27 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] B = {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+        };
+
+        // Result matrix
+        int rowsA = A.length;       // Number of rows in A
+        int colsA = A[0].length;   // Number of columns in A
+        int colsB = B[0].length;   // Number of columns in B
+        int[][] C = new int[rowsA][colsB];
+
+        // Matrix multiplication
+        TODO
+
+        // Print result matrix
+        TODO
+
+    }
+}
